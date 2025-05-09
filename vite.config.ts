bb8283a1df7aa01185264f7451e6720db2804eaf
juniorbrindou <225juniorbrindou@gmail.com>
2025-05-09 00:58:51 +0000

    feat: add sitemap.xml for improved SEO and site indexing

--- vite.config.ts
+++ vite.config.ts
@@ -2,37 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react({
-  })],
-  build: {
-    target: 'esnext',
-    minify: 'terser', // Minification plus agressive
-    cssMinify: true,
-    sourcemap: false, // Désactive les sourcemaps en prod
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            // Crée des chunks séparés pour les grosses dépendances
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'vendor-react';
-            }
-            if (id.includes('framer-motion')) {
-              return 'vendor-framer';
-            }
-            return 'vendor'; // Toutes les autres dépendances
-          }
-        },
-      },
-    },
-  },
+  plugins: [react()],
   optimizeDeps: {
-    include: [
-      'react',
-      'react-dom',
-      'react-router-dom',
-      // Ajoute d'autres dépendances fréquemment utilisées
-    ],
     exclude: ['lucide-react'],
   },
 });
