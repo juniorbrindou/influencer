8e643eab7303fecc4562c062d3b273a3b9bb2616
juniorbrindou <225juniorbrindou@gmail.com>
2025-05-26 23:24:10 +0000

    refactor: comment out unused results fetching interval logic in ResultsChart component

--- src/components/ResultsChart.tsx
+++ src/components/ResultsChart.tsx
@@ -19,13 +19,13 @@ const ResultsChart: React.FC<ResultsChartProps> = ({ categoryId, categoryName })
     return 1; // Niveau de base
   };
 
-  useEffect(() => {
-  const interval = setInterval(() => {
-    fetchResults(categoryId);
-  }, 1000 * 30); // Toutes les 30 secondes
+//   useEffect(() => {
+//   const interval = setInterval(() => {
+//     fetchResults(categoryId);
+//   }, 1000 * 30); // Toutes les 30 secondes
 
-  return () => clearInterval(interval);
-}, [categoryId, fetchResults]);
+//   return () => clearInterval(interval);
+// }, [categoryId, fetchResults]);
 
   const styleLevel = getStyleLevel();
 
